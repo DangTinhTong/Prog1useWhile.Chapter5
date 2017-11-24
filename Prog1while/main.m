961d204677595bfcc71803ae4e3559bdd6ec2a73
admin <dangtinhtong@gmail.com>
2017-11-25 00:27:14 +0700

    Initial Commit

---
+++ Prog1while/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Prog1while
+//
+//  Created by admin on 11/25/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
